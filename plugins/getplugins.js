7f632ddd23bd75cdbcfbe0955afc4496bfff8be9
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-04-15 13:04:57 +0800

    Update getplugins.js

--- plugins/getplugins.js
+++ plugins/getplugins.js
@@ -1,19 +1,14 @@
-const fs = require('fs')
-const path = require('path')
 let handler = async (m, { usedPrefix, command, text }) => {
-    if (!text) throw `where is the text?\n\nexempel: ${usedPrefix + command} menu`
-    const filename = path.join(__dirname, `./${text}${!/\.js$/i.test(text) ? '.js' : ''}`)
-    const listPlugins = fs.readdirSync(path.join(__dirname)).map(v => v.replace(/\.js/, ''))
-    if (!fs.existsSync(filename)) return m.reply(`
-'${filename}' not found!
-${listPlugins.map(v => v).join('\n').trim()}
-`.trim())
-    m.reply(fs.readFileSync(filename, 'utf8'))
+    let ar = Object.keys(plugins)
+    let ar1 = ar.map(v => v.replace('.js', ''))
+    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} creator`
+    if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    m.reply(require('fs').readFileSync('./plugins/' + text + '.js', 'utf-8'))
 }
-handler.help = ['getplugin'].map(v => v + ' [filename]')
+handler.help = ['getplugin'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(getplugin|get ?plugin)$/i
+handler.command = /^(getplugin|gp)$/i
 
-handler.rowner = true
+handler.owner = true
 
 module.exports = handler
