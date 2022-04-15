756f84450bba4111f956c0a0c918be546b8c68a9
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-04-16 02:45:44 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,4 +1,4 @@
-let moment = require('moment-timezone')
+/*let moment = require('moment-timezone')
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let fetch = require('node-fetch')
@@ -54,4 +54,21 @@ handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok)$/i
 
+module.exports = handler*/
+
+const hxz = require("hxz-api")
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+let p = await  hxz.ttdownloader(args[0])
+const { nowm, wm, audio } = p
+// made by aine
+ conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*© Nevt*`, m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok|tiktokdl)$/i
+handler.limit = true
+handler.group = true
 module.exports = handler
+
