a9325c9c8d850c1f2f586bfc6663b8c289b5f2c5
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-04-15 14:54:17 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -328,8 +328,6 @@ const defaultMenu = {
 │✾ *Mode:* ${global.opts['self'] ? 'Self' : 'publik'}
 │✾ *Runtime:* %uptime
 │✾ *Database:* %rtotalreg dari %totalreg
-│✾ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* *Chat Terbanned*
-│✾ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ╰❑
 ╭─❑ 「 INFO USER 」 ❑──
 │ ✾ *Name:* %name
