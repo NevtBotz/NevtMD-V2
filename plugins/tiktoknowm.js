8210bbdecffe36afb072783fcb98793845c2c64c
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-04-15 14:00:11 +0800

    Update tiktoknowm.js

--- plugins/tiktoknowm.js
+++ plugins/tiktoknowm.js
@@ -1,10 +1,15 @@
-
 const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-let p = await  hxz.ttdownloader(text)
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+let p = await  hxz.ttdownloader(args[0])
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'nih', m)
+// made by aine
+ conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*© Nevt*`, m)
 }
-handler.command = ['tiktoknowm']
+handler.help = ['tiktoknowm <url>']
+handler.tags = ['downloader']
+
+handler.command = /^(tik|tt|tiktoknowm)$/i
+handler.premium = true
+handler.limit = true
+
 module.exports = handler
