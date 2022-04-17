c778282da3fe2b8ac8e5c509a63171bd687b0c69
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-04-17 18:41:45 +0800

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,22 +1,27 @@
-const { toPTT } = require('../lib/converter')
-const { MessageType } = require('@adiwajshing/baileys-md')
+const { toAudio, toPTT } = require('../lib/converter')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
-  let media = await q.download()
-  let audio = await toPTT(media, 'mp4')
- 
-//conn.sendFile(m.chat, audio,  0,0,m,true, {ptt:true })
- conn.sendMessage(m.chat, {audio:audio}, {
-    quoted: m,
-    ptt: true
-  })
+  if (/mp3|a(udio)?$/i.test(command)) {
+    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
+    let media = await q.download()
+    if (!media) throw 'Media tidak dapat diunduh'
+    let audio = await toAudio(media, 'mp4')
+    if (!audio.data) throw 'Gagal melakukan konversi.'
+    await conn.sendFile(m.chat, audio.data, 'file.mp3', '', m, 0, { mimetype: 'audio/mp4', asDocument: global.db.data.chats[m.chat].useDocument })
+  }
+  if (/vn|ptt$/i.test(command)) {
+    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
+    let media = await q.download()
+    if (!media) throw 'Media tidak dapat diunduh'
+    let audio = await toPTT(media, 'mp4')
+    if (!audio.data) throw 'Gagal melakukan konversi.'
+    await conn.sendFile(m.chat, audio.data, 'file.mp4', '', m, 1, { mimetype: 'audio/mp4' })
+  }
 }
-handler.help = ['tomp3 (reply)']
-handler.tags = ['tools']
-
-handler.command = /^to(mp3|a(udio)?)$/i
+handler.help = ['tomp3', 'tovn']
+handler.tags = ['maker']
+handler.command = /^to(mp3|a(udio)?|vn|ptt)$/i
 
 module.exports = handler
